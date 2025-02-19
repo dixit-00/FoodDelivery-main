65042c2ced81682c62e98dfd8486726f4393a3d5
dixit-99 <malviyadixit92@gmail.com>
2025-02-19 17:30:34 +0530

    today

---
+++ FoodDelivery/UICommon/Addcart.swift
@@ -0,0 +1,18 @@
+//
+//  Addcart.swift
+//  FoodDelivery
+//
+//  Created by student on 19/02/25.
+//
+
+import SwiftUI
+
+struct Addcart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Addcart()
+}
---
+++ FoodDelivery/View/HomeView/CartDetails.swift
@@ -0,0 +1,18 @@
+//
+//  CartDetails.swift
+//  FoodDelivery
+//
+//  Created by student on 18/02/25.
+//
+
+import SwiftUI
+
+struct CartDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartDetails()
+}
