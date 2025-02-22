78fbc4385af56902484ebb089f2931e83be4e0fb
dixit-99 <malviyadixit92@gmail.com>
2025-02-22 16:30:49 +0530

    today

---
+++ FoodDelivery/View/HomeView/AccountView/chatbotview.swift
@@ -0,0 +1,18 @@
+//
+//  chatbotview.swift
+//  FoodDelivery
+//
+//  Created by student on 21/02/25.
+//
+
+import SwiftUI
+
+struct chatbotview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    chatbotview()
+}
